858a4823d69248425bfb561db07becc00cb27553
Marsha Battee <marsha@sanesuite.com>
2019-07-16 16:23:19 -0600

    writing for author id and author hash

--- php/classes/foo.php
+++ php/classes/foo.php
@@ -49,5 +49,85 @@ class	Author {
 	 */
 	private $authorUsername;
 
+
+
+	/**
+	 * Accessor method for authorId
+	 * @return int value of authorId
+	 */
+
+	public function getAuthorId() {
+		return($this->getAuthorId);
+	}
+	/**
+	 * Mutator method for authorId allows author to set a new Id for author.
+	 *
+	 * @param int $newAuthorId value for author Id
+	 * @throws UnexpectedValueException if $newAuthorId is not an interger.
+	 */
+
+	public function setAuthorId($newAuthorId) {
+		//verify the authorId is valid
+		$newAuthorId = filter_var($newAuthorId, FILTER_VALIDATE_INT);
+		if($newAuthorId === false) {
+			throw(new UnexpectedValueException("author ID is not a valid integer"));
+		}
+		//convert and store the author id
+		$this->authorId = intval($newAuthorId);
+
+	}
+
+	/**
+	 * Accessor method for avatarUrl
+	 */
 }
+
+	/**
+	 * Accessor method for authorActivationToken
+	 */
+
+	/**
+	 * Accessor method for authorEmail
+	 */
+
+	/**
+	 * Accessor method for authorHash
+	 * @return string value of author hash
+	 **/
+	public function getAuthorHash(): string {
+		return $this->authorHash;
+		}
+
+	/**
+	 * Mutator method for author hash allows author another hash
+	 *
+	 * @param string is a new author hash value for authorHash
+	 * @throws \InvalidArgumentException if the hash is not secure
+	 * @throws \RangeException if the hash is not 128 characters
+	 * @throws \TypeError if profile hash is not a string
+	 **/
+
+	public function setAuthorHash (string $newAuthorHash): void {
+		//enforces that the author hash is properly formatted
+		$newAuthorHash = trim($newAuthorHash);
+		if(empty($newAuthorHash) === true) {
+			throw(new \InvalidArgumentException("author password hash is empty or insecure"));
+		}
+		//enforces that the hash is an Argon hash
+		$authorHashInfo = password_get_info($newAuthorHash);
+		if($authorHashInfo["algoName"] !== "argon2i") {
+			throw(new \InvalidArgumentException("author hash is not a valid hash"));
+		}
+		//enforces that the hash is exactly 97 characters.
+		if(strlen($newAuthorHash) !== 97) {
+			throw(new \RangeException("profile hash must be 97 characters"));
+		}
+		//stores the hash
+		$this->authorHash = $newAuthorHash;
+};
+
+
+	/**
+	 * Accessor method for authorUsername
+	 */
 ?>
